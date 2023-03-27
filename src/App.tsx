f7a753b819abc883e298cab445d33867e2131ff4
Svitlana Yudina <osbka31@gmail.com>
2023-03-27 10:32:33 +0300

    button component was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.scss';
 import { BackGroundPicture } from './components/BackGroundPicture';
+import { PrimaryButton } from './components/PrimaryButton';
 
 export const App: React.FC = () => {
   return (
     <div className="app">
       <div className="app__content">
-        <BackGroundPicture />
-        hello
+      <BackGroundPicture />
+        <div className="block">
+          <PrimaryButton
+            type="choice"
+            text="Я мужчина"
+            image="./images/m_icon.svg"
+          />
+          <PrimaryButton type="quiz" text="дружба и общение"/>
+          <PrimaryButton type="create" text="Создать"/>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+import React from 'react';
+import { ButtonStyles } from '../../types/types';
+import styles from './PrimaryButton.module.scss';
+
+type Props = {
+  type: ButtonStyles,
+  text: string,
+  image?: string;
+};
+
+const BUTTON_STYLES = {
+  choice: styles.sexChoice,
+  quiz: styles.quizAnswers,
+  create: styles.createUser,
+}
+
+export const PrimaryButton: React.FC<Props> = ({ type, text, image }) => {
+  return (
+    <button
+      type="submit"
+      className={classNames(
+        styles.default,
+        BUTTON_STYLES[type],
+        { [styles.withIcon]: !!image },
+      )}
+    >
+      {image && (
+        <img className={styles.icon} src={image} alt="person" />
+      )}
+      <span>{text}</span>
+    </button>
+  );
+}
