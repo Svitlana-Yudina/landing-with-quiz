06ff5104bf23990e0269992a7e70ef30a2c3bf3d
Svitlana Yudina <osbka31@gmail.com>
2023-03-27 18:32:00 +0300

    comments were added

--- src/modules/Main/Main.tsx
+++ src/modules/Main/Main.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { BackGroundPicture } from '../../components/BackGroundPicture';
+import { CommentsList } from '../../components/CommentsList';
 import { PrimaryButton } from '../../components/PrimaryButton';
 import styles from './Main.module.scss';
 
 export const Main: React.FC = () => {
   return (
-    <div className={styles.main}>
-      <BackGroundPicture />
+    <>
+      <div className={styles.main}>
+        <BackGroundPicture />
 
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          Наибольшая база анкет для знакомств
-        </h1>
-        <PrimaryButton type="sex" text="Я мужчина" image="./images/m_icon.svg"/>
-        <PrimaryButton type="sex" text="Я женщина" image="./images/f_icon.svg"/>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            Наибольшая база анкет для знакомств
+          </h1>
+          <PrimaryButton
+            type="sex"
+            text="Я мужчина"
+            image="./images/m_icon.svg"
+          />
+          <PrimaryButton
+            type="sex"
+            text="Я женщина"
+            image="./images/f_icon.svg"
+          />
+        </div>
       </div>
-    </div>
+
+      <CommentsList />
+    </>
   );
 };
