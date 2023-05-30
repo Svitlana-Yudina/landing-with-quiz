11df9437955542156439e6bc9c9a4488ab2103ba
Svitlana Yudina <osbka31@gmail.com>
2023-05-30 19:54:08 +0300

    add pwa

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,25 @@
+import React, { memo } from 'react';
+import styles from './Button.module.scss';
+
+const Button = () => {
+  return (
+    <button className={styles.butt} onClick={() => {
+      if ('serviceWorker' in navigator) {
+        // window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then(registration => {
+            // eslint-disable-next-line no-console
+            console.log('Service Worker is registered! ', registration.scope);
+          })
+          .catch(err => {
+            // eslint-disable-next-line no-console
+            console.log('Registration failed  ', err);
+          });
+        // });
+      }
+    }
+    }>hello</button>
+  );
+};
+
+export default memo(Button);
