6c9e95bc9670681365a208fa4e8917b56e07f0a4
Svitlana Yudina <osbka31@gmail.com>
2023-05-30 20:00:30 +0300

    add pwa - 2

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,7 +6,7 @@ const Button = () => {
     <button className={styles.butt} onClick={() => {
       if ('serviceWorker' in navigator) {
         // window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
           .then(registration => {
             // eslint-disable-next-line no-console
             console.log('Service Worker is registered! ', registration.scope);
