1ffae63cbfbc12cd08a7cfe72fa383e8a517538c
svitlana-yudina-quarks-tech <svitlana.yudina@remote.quarks.tech>
2025-07-26 16:58:23 +0300

    CRO-123: replace one secret

--- src/modules/Quiz/Quiz.tsx
+++ src/modules/Quiz/Quiz.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const Quiz: React.FC<Props> = ({ question, setStep }) => {
   const handleClickQuiz = () => {
-    setStep(currStep => currStep + 1);
+    setStep(myStep => myStep + 1);
   };
 
   return (
